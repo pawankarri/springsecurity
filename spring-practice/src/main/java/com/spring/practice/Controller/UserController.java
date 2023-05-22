6dc1bd80f75bff0472b729990bb97db6471370d6
pawankarri <121856461+pawankarri@users.noreply.github.com>
2023-05-22 18:48:15 +0530

    Add files via upload

--- spring-practice/src/main/java/com/spring/practice/Controller/UserController.java
+++ spring-practice/src/main/java/com/spring/practice/Controller/UserController.java
@@ -13,13 +13,20 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.parameters.P;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/users")
@@ -31,10 +38,8 @@ public class UserController {
     private JwtServiceImpl jwtServiceImpl;
     @Autowired
     private AuthenticationManager authenticationManager;
-    @Autowired
-    private UserInfoUserDetails userInfoUserDetails;
-    @Autowired
 
+    @Autowired
     private UserInfoUserDetailsService userInfoUserDetailsService;
     @GetMapping("/getAll")
     @PreAuthorize("hasAuthority('ROLE_ADMIN')")
@@ -63,22 +68,51 @@ public class UserController {
     {
         return userService.addUser(userInfo);
     }
-@PostMapping("/authenticate")
-    public ResponseEntity<AuthResponse> authenticateAndGetToken(@RequestBody AuthRequest authRequest) throws UsernameNotFoundException {
-    Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
-    if (authentication.isAuthenticated()) {
-        //UserDetails userDetails = this.userInfoUserDetailsService.loadUserByUsername(authRequest.getUsername());
-        String token=this.jwtServiceImpl.generateToken(authRequest.getUsername());
-        AuthResponse authResponse=new AuthResponse();
-        authResponse.setUsername(this.jwtServiceImpl.getUserNameFromToken(token));
-        authResponse.setToken(token);
-        authResponse.setTokenExpirationTime(this.jwtServiceImpl.getExpirationDateFromToken(token).toString());
-        authResponse.setMessage("success");
-        return new ResponseEntity<AuthResponse>(authResponse, HttpStatus.OK);
-    } else {
-        throw new UsernameNotFoundException("invalid user request!");
-
+//@PostMapping("/authenticate")
+//    public ResponseEntity<AuthResponse> authenticateAndGetToken(@RequestBody AuthRequest authRequest) throws UsernameNotFoundException {
+//    Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
+//    if (authentication.isAuthenticated()) {
+//       // UserDetails userDetails = this.userInfoUserDetailsService.loadUserByUsername(authRequest.getUsername());
+//        String token=this.jwtServiceImpl.generateToken(authRequest.getUsername());
+//        AuthResponse authResponse=new AuthResponse();
+//        authResponse.setUsername(this.jwtServiceImpl.getUserNameFromToken(token));
+//        authResponse.setToken(token);
+//        authResponse.setTokenExpirationTime(this.jwtServiceImpl.getExpirationDateFromToken(token).toString());
+//        authResponse.setMessage("success");
+//
+//        return new ResponseEntity<AuthResponse>(authResponse, HttpStatus.OK);
+//    } else {
+//        throw new UsernameNotFoundException("invalid user request!");
+//
+//    }
+//
+//}
+    @PostMapping("/authenticate")
+    public ResponseEntity<Map<String,Object>> login(@RequestBody AuthRequest authRequest) throws Exception {
+        this.authenticate(authRequest.getUsername(),authRequest.getPassword());
+        UserDetails userDetails=this.userInfoUserDetailsService.loadUserByUsername(authRequest.getUsername());
+        String token=this.jwtServiceImpl.generateToken(String.valueOf(userDetails));
+        Map<String,Object> map=new HashMap<>();
+        map.put("token",token);
+        map.put("tokenExpTime",this.jwtServiceImpl.getExpirationDateFromToken(token).toString());
+        map.put("message","success");
+        map.put("userName",userDetails.getUsername());
+        map.put("userRole",userDetails.getAuthorities());
+        return new ResponseEntity<Map<String,Object>>(map,HttpStatus.OK);
     }
 
-}
+    private void authenticate(String username,String password) throws Exception {
+        UsernamePasswordAuthenticationToken authenticationToken=new UsernamePasswordAuthenticationToken(username,password);
+        try {
+            this.authenticationManager.authenticate(authenticationToken);
+        }
+        catch (DisabledException e)
+        {
+            throw new DisabledException("user is disabled");
+        }
+        catch (BadCredentialsException e)
+        {
+            throw new BadCredentialsException("bad credentials");
+        }
+    }
 }
